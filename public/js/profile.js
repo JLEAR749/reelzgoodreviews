6050309d96e6be41e5b6b8fd471017680973ef6d
J0J0 <iditaroddream@gmail.com>
2023-02-09 21:25:16 -0600

    working on handlebars and profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,3 +1,45 @@
-// may not need this file
-// only will need this if we decided to create a profile page.
-// save for later
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Profile extends Model { }
+
+Profile.init(
+    {
+        id: {
+            type: DataTypes,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            },
+            },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isPassword: true
+            }
+        }
+        },
+    },
+{
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "profile",
+    }
+);
+
+module.exports = Profile;
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const path = require("path");
+ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
