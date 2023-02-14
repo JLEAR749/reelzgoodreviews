28a83786c2f7f18eb0fd8503bc2ffc5b32e81900
dylanquaale <dylanquaale33@gmail.com>
2023-02-14 09:35:13 -0600

    good

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,12 +4,9 @@ const userRoutes = require('./users-routes');
 const movieRoutes = require('./movie-routes');
 const reviewRoutes = require('./review-routes');
 
-
 router.use('/users', userRoutes);
 router.use('/movies', movieRoutes);
 router.use('/reviews', reviewRoutes);
-
-
 
 router.use((req, res) => {
     res.status(404).end();
