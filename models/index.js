229fd6ccf61d8e2f70f119f157b2172842dc2662
swmcguire <swmcguire1@comcast.net>
2023-02-13 19:15:28 -0600

    Added movie seeds and movie routes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const Review = require('./Review');
 const User = require('./User');
 const Post = require('./Post');
+const Movie = require('./Movie');
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -27,4 +28,23 @@ Review.belongsTo(Post, {
     foreignKey: 'post_id'
 })
 
-  module.exports = { Review, User, Post};
+
+//------------------------UPDATED MOVIE CODE
+User.hasMany(Movie, {
+  foreignKey: 'user_id'
+});
+
+Movie.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Movie.hasMany(Review, {
+  foreignKey: 'post_id'
+});
+
+Review.belongsTo(Movie, {
+  foreignKey: 'post_id'
+})
+
+
+  module.exports = { Review, User, Post, Movie};
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User, Post, Movie } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
+const movieData = require('./movie-seeds.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,13 +13,17 @@ const seedDatabase = async () => {
     returning: true,
   });
 
+  const movies = await Movie.bulkCreate(movieData, {
+    individualHooks: true, 
+    returning: true,
+  });
+
   for (const post of postData) {
     await Post.create({
       ...post,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
-  }
-  
+  };
 
   process.exit(0);
 };
